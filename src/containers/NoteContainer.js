cdd32472c375340757f1db37c8eab3f999ae2081
Erin Gurley <erinmgurley@gmail.com>
2019-09-03 16:12:21 -0700

    created NoteContainer and rendering NoteForm and Notes

---
+++ src/containers/NoteContainer.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Notes from '../components/notes/Note';
+import { connect } from 'react-redux';
+import { NoteForm } from '../components/forms/NoteForm';
+import { fetchNote, createNote } from '../actions/noteAction';
+import { getNotesList } from '../selectors/noteSelector';
+
+class NotesContainer extends Component { 
+  static propTypes = { 
+    notes: PropTypes.array,
+    fetch: PropTypes.func,
+    createNote: PropTypes.func
+  };
+
+  componentDidMount() {
+    this.props.fetch();
+  }
+
+  render() {
+    const { notes, createNote } = this.props;
+    return (
+    <>
+      <NoteForm createNote={createNote} />
+      <Notes notes={notes} />
+    </>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  notes: getNotesList(state)
+});
+
+const mapDispatchToProps = dispatch => ({
+  fetch() {
+    dispatch(fetchNote());
+  },
+  createNote(note) {
+    dispatchEvent(createNote(note));
+  }
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(NotesContainer);
